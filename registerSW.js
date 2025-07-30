b64d995627da1091b896632340597d23ec9b8712
Ahmad Faisal <ahmadfaisall.020597@gmail.com>
2025-07-30 09:30:31 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/testingjamkrida/sw.js', { scope: '/testingjamkrida/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
